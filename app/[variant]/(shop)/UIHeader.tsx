99b0d4cc18d75d1dd6c7bccf25b6adb86edcb5ad
Razvan Soare <soare.razvan94@gmail.com>
2023-01-27 08:59:01 +0000

    test

--- app/[variant]/(shop)/UIHeader.tsx
+++ app/[variant]/(shop)/UIHeader.tsx
@@ -9,20 +9,18 @@ export const UIHeader = (p: any) => {
       <nav className="flex gap-4" style={{ display: "flex", gap: 16 }}>
         {counter}
         <button onClick={() => setCounter((c) => c + 1)}>hit</button>
-        <Link prefetch={false} href="/">
-          Home
-        </Link>
+        <Link href="/">Home</Link>
         <Link href="shop">shop</Link>
         <Link href="product">product</Link>
         <Link href="/who-we-are">who we are</Link>
         <Link href="/about">about</Link>
         <Link href="/contact">contact</Link>
-        <UILink to="/">Home v2</UILink>
+        {/* <UILink to="/">Home v2</UILink>
         <UILink to="shop">shop v2</UILink>
         <UILink to="product">product v2</UILink>
         <UILink to="/who-we-are">who we are v2</UILink>
         <UILink to="/about">about v2</UILink>
-        <UILink to="/contact">contact v2</UILink>
+        <UILink to="/contact">contact v2</UILink> */}
       </nav>
     </header>
   );
